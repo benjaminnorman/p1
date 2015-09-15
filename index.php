1fda5e8cac94b44c1204488512eb00dd79749c0b
Benjamin Norman <bnorman@g.harvard.edu>
2015-09-15 18:24:47 -0500

    Created main page for P1. Includes links to CDN's for Bootstrap libraries.

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Ben Norman's Landing Page</title>
+
+    <!-- Bootstrap -->
+    <!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <!--<link href="css/bootstrap.min.css" rel="stylesheet">-->
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Benjamin Norman</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">		
+		<li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Project 2 <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Site</a></li>
+            <li><a href="https://github.com/benjaminnorman/p2">GitHub Repository</a></li>
+          </ul>
+        </li>
+		
+		<li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Project 3 <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Site</a></li>
+            <li><a href="https://github.com/benjaminnorman/p3">GitHub Repository</a></li>
+          </ul>
+        </li>
+
+		<li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Project 4 <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Site</a></li>
+            <li><a href="https://github.com/benjaminnorman/p4">GitHub Repository</a></li>
+          </ul>
+        </li>
+
+      </ul>
+      
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+  </head>
+  <body>
+  	<div class="jumbotron">
+			<div align="center">
+		  		<h2>Hello, internet. My name is Ben, and I will be your guide.</h2>
+		  		<img src="benpic.jpg" alt="Benjamin Norman">
+			</div>
+		</div>
+	<div align="center">
+    	<h2>Allow me to introduce myself...</h2>
+    	<div class="panel panel-primary">
+		  <div class="panel-heading"><h3>Who I am and what I do</h3></div>
+		  <div class="panel-body">
+	    	<p>
+	    		My education includes a bachelors in Industrial Engineering, but currently I work as a Global IT Quality Manager for a large global company.
+	    		My work currently is focused on identifying and gathering details about the top quality issues facing users within the company, and connecting with the domain experts to get the issues resolved quickly.
+	    		I use various techniques to measure the satisfaction of our internal users, and utilize my background in technology to understand what the reported issues are referring to and identify who to get in contact with.
+	    	</p>
+		  </div>
+		</div>
+
+    	<div class="panel panel-success">
+		  <div class="panel-heading"><h3>Why am I taking this course</h3></div>
+		  <div class="panel-body">
+	    	<p>
+	    		I'm taking this course to deepen my understanding of how web applications work on the back end.
+	    		Most of the interfacing that users do with the internet do these days is through web applications such as Facebook, YouTube, etc, and I'm intrigued by how they work under the hood.
+	    		Additionally, since PHP is such a commonly deployed technology that many of the modern websites rely upon, I was interested to learn more about it.
+	    	</p>
+		  </div>
+		</div>
+
+
+    	<div class="panel panel-info">
+		  <div class="panel-heading"><h3>Previous Programming Experience</h3></div>
+		  <div class="panel-body">
+	    	<p>
+    			Having studied engineering, my background in programming is not as extensive as someone who might have studied computer science or something more directly related with programming
+    			However, I have been learning small bits of programming since I was a child, and in the past few years I have really advanced in my coding abilities.
+    			The most quickly I have ever improved my programming skills was when I took CS50 several semesters ago. The course covered typical computer science principles in C, then branched into things like HTML, PHP, SQL, and more.
+    			That class prepared me for the Java for Distributed Programming class, and several of the more programming intensive classes I have taken since then.
+	    	</p>
+    		<p>
+    			My strongest programming skills are probably in Javascript, but I find that once I dig into a language and start using it more frequently then that language becomes my language of choice.
+    			It is quite easy to forget the syntax and some of the implementation details associated with a programming language if you don't use it consistantly, which is why I like continually taking programming  classes - as to not lose my abilities.	
+			</p>   
+		  </div>
+		</div>
+
+    	<div class="panel panel-warning">
+		  <div class="panel-heading"><h3>Platforms I am using</h3></div>
+		  <div class="panel-body">
+	    	<p>
+	    		In my daily computing for personal use I use a Ubuntu 14.04 partition on my laptop, which I very much enjoy and prefer over Windows for a variety of reasons. I like being able to hit my favorite keyboard combo (Ctrl-Alt-T) to summon a terminal window at a moments notice.
+    			Using the command line gives the feeling to the user that they are more connected to the machine, as it strips away the GUI interface and relies on the raw abilities of the user. It's also just fun, especially with this super clicky mechanical keyboard!
+    			For the assignments in this class I will be using my Windows 10 partition (which does not function entirely correctly quite yet), CMDR, SublimeText3, and perhaps PHPStorm as I found I really liked using Webstorm for one of my Javascript intensive classes and it's quite similar.
+	    	</p>
+		  </div>
+		</div>
+
+
+    	<div class="panel panel-danger">
+		  <div class="panel-heading"><h3>Additional Information</h3></div>
+		  <div class="panel-body">
+	    	<p>
+	    		My favorite genre of music is melodic death metal, a good sample of which can be found here: 
+    			<p><a class="btn btn-danger btn-lg" href="https://www.youtube.com/watch?v=xdCSJaQjfXw" role="button">Click for awesome tunes!</a></p>
+	    	</p>
+		  </div>
+		</div>
+
+    </div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  </body>
+</html>
